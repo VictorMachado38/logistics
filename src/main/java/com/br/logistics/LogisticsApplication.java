10cb3a4e44d218c800297040ebda948296cb112a
victor <victor.pedrozo@hotmail.com>
2023-06-16 00:25:59 -0300

    feat: new feature

--- src/main/java/com/br/logistics/LogisticsApplication.java
+++ src/main/java/com/br/logistics/LogisticsApplication.java
@@ -3,12 +3,18 @@ package com.br.logistics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableWebSecurity
+//@EnableSwagger2
 public class LogisticsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogisticsApplication.class, args);
 	}
+
 
 }
---
+++ src/main/java/com/br/logistics/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.br.logistics;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**"
+    };
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(AUTH_WHITELIST);
+    }
+}
